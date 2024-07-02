504464f6dd9428ec05e4fa3f19255b79403683f6
cthoman1 <cthoman24@gmail.com>
2024-07-02 16:09:39 -0400

    Scrapped the way that I was initially comparing trajectories and started using dplyr in a different way. Also set up functions for finding Euclidean distance, etc

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -5,36 +5,93 @@ load("data/cleaned/cleaned_events.rdata")
 library(dplyr)
 library(tidyr)
 library(purrr)
-filtered_df <- cleaned_race_results %>%
-  group_by(athlete_id, event) %>%
-  filter(n() >= 3) %>%
-  ungroup()
-# Selects only those results for which the athlete has done it 3 or more times.
-# Any less and the trajectories would not be very meaningful.
 
 contains_letters <- function(result) {
   grepl("[[:alpha:]]", result)
 }
-#Function to determine whether a result contains letters (DQ, DNF, etc).
+# Function to determine whether a result contains letters (DQ, DNF, etc).
 
-trajectory_tables <- filtered_df %>%
-  group_by(athlete_id, event) %>%
-  arrange(race_date) %>%
-  summarise(
-    results = list(result),
-    race_dates = list(format(race_date, "%m/%d/%y")),  
-    .groups = 'drop') 
-# Uses the filtered database to create chronological result lists for each athlete-event pairing.
+euclidean_dist <- function(v1, v2) {
+  if (length(v1) == length(v2)) {
+    distance <- sqrt((v1-v2)^2)
+    return(sum(distance))
+  }
+  else {
+    print("The operation could not be completed because the vectors have differnet lengths.")
+  }
+}
+# Finds point-by-point Euclidean distance between two vectors.
 
-filtered_trajectory_tables <- trajectory_tables %>%
-  rowwise() %>%
-  mutate(
-    results = list(as.numeric(results[!contains_letters(results)])),
-    race_dates = list(race_dates[!contains_letters(results)])
-  ) %>%
-  ungroup() %>%
-  filter(map_int(results, length) > 2) 
-# Returns the trajectory tables but including only numeric results.
+minimize_distance <- function(v1,v2) {
+  if (length(v1) == length(v2)) {
+    return(1)
+  }
+  if (length(v1) > length(v2)) {
+    v1 <- v1
+    v2 <- v2
+    index <- 1
+  } else {
+    v1 <- v2
+    v2 <- v1
+    index <- 2
+  }
+  max_length <- length(v1)
+  min_length <- length(v2)
+  minimum_distance <- Inf
+  for (i in 1:(length(v1) - length(v2) + 1)) {
+    dist <- euclidean_dist(v2, v1[i:(i + (min_length - 1))])
+    if (dist < minimum_distance) {
+      minimum_distance <- dist
+      pos <- i
+    }
+  }
+  return(list(index,minimum_distance,pos))
+}
+# This function returns the position on the larger vector at which the smaller vector can be placed to minimize Euclidean distance.
+# It also returns which of the two is the larger.
+
+athlete_trajectory <- function(id1,id2) {
+  trajectory <- cleaned_race_results %>%
+    filter(athlete_id == id1 & event == id2) %>%
+    arrange(race_date) %>%
+    select(-athlete_id, -athlete_id, -event, -race_date) %>%
+    mutate(result = as.numeric(result))
+  return(trajectory[,1])
+}
+# Takes an athlete ID and event code and returns a vector of the athlete's results for that event.
+
+compare_trajectory <- function(id1,id2) {
+  event_subset <- cleaned_race_results %>%
+    filter(event==id2) %>%
+    group_by(athlete_id) %>%
+    filter(n() > 2) %>%
+    filter(!contains_letters(result)) %>%
+    summarize() %>%
+    pull("athlete_id")
+  if (!(id1 %in% event_subset)) {
+    print("There are not enough results for the given athlete-event combination (3>) to perform the operation.")
+  }
+  else {
+    event_subset <- data.frame(athlete_id = event_subset, index = NA, dist = NA, pos = NA)
+    for (i in event_subset) {
+      compare_results <- minimize_distance(
+        athlete_trajectory(i,id2),
+        athlete_trajectory(id1,id2)
+      )
+      event_subset$index[i] <- compare_results[1]
+      event_subset$dist[i] <- compare_results[2]
+      event_subset$pos[i] <- compare_results[3]
+    event_subset %>%
+      arrange(dist) %>%
+      head(10)
+    }
+    return(event_subset)
+  }
+}
+
+compare_trajectory(7820846,13)
+athlete_trajectory(7820846,10)
+athlete_trajectory(7820844,10)
+minimize_distance(athlete_trajectory(7820846,10),athlete_trajectory(7820844,10))
 
 
-write.csv(events,file="events.csv")
