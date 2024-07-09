ae524b490dc1db18509acaf8d0420596fa23b6f8
cthoman1 <cthoman24@gmail.com>
2024-07-09 10:14:24 -0400

    started on API

---
+++ scripts/plumber.R
@@ -0,0 +1,61 @@
+library(plumber)
+library(plumberDeploy)
+source("analysis.R")
+
+#* @apiTitle Race Time Analytics API
+#* @apiDescription API for interacting with race time analytics
+
+#* Compare trajectory
+#* @param id1 athlete ID
+#* @param id2 event code
+#* @param first_year start year
+#* @param last_yaer end year
+#* @param min_events minimum events
+#* @param recency_bias a recency bias scalar
+#* @get /compare_trajectory
+
+compare_trajectory <- function(id1,id2,first_year, last_year, min_events,recency_bias) {
+  event_subset <- cleaned_race_results %>%
+    filter(event==id2) %>%
+    group_by(athlete_id) %>%
+    filter(!contains_letters(result)) %>%
+    filter(n() >= min_events) %>%
+    mutate(year = as.integer(substr(race_date, 1, 4))) %>%
+    select(-race_date) %>%
+    filter(year >= first_year & year <= last_year) %>%
+    summarize() %>%
+    pull("athlete_id")
+  # This creates 'event_subset', a list of athlete_ids in the set with 3+ non-text results for the event.
+  if (!(id1 %in% event_subset)) {
+    print("The athlete ID given does not fit into the parameters given.")
+  }
+  else {
+    event_subset <- data.frame(athlete_id = event_subset, index = NA, dist = NA, pos = NA, dists = NA)
+    for (i in seq_along(event_subset$athlete_id)) {
+      compare_results <- minimize_distance(
+        athlete_trajectory(event_subset$athlete_id[i],id2),
+        athlete_trajectory(id1,id2),
+        recency_bias
+      )
+      event_subset$index[i] <- as.integer(compare_results[1])
+      event_subset$dist[i] <- as.numeric(compare_results[2])
+      event_subset$pos[i] <- as.integer(compare_results[3])
+      event_subset$dists[i] <- as.vector(compare_results[4])
+      # This creates a three-item list for each other athlete in the data set.
+      # The three items are the three outputs from the distance minimization function.
+      closest_trajectories <- event_subset %>%
+        arrange(dist) %>%
+        slice(2:11)
+    }
+    return(closest_trajectories)
+    # This appends the three outputs from distance minimization to the IDs in 'event_subset'.
+    # It then sorts them based on distance and takes the top ten (non-self) results.
+  }
+}
+
+# This function takes a few inputs.
+# id1 and id2 correspond to athlete_id and event code respectively.
+# first_year and last_year set the time range of athletes surveyed.
+# min_events sets a minimum for length of the vectors compared.
+# recency_bias determines to what extent recent events are favored on a power-law scale.
+# It outputs a table of the ten closest non-self trajectories with index, dist, pos, and dists.
